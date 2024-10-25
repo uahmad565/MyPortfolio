90e252e91a8d96684611bbfa4775346c0d27bbeb
Usman Kabeer <uahmad565565@outlook.com>
2024-10-25 16:35:27 +0500

    Add project files.

---
+++ ChatBotDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChatBotDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
